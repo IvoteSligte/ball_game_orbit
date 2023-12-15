7aa8cc15697b80370c4d29c77c1dd4f8a9e221f3
Ivo te Sligte <ivotesligte@gmail.com>
2023-12-15 21:25:22 +0100

    broken commit

--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,96 @@ struct BallGamePlugin;
 
 impl Plugin for BallGamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(schedule, systems)
+        app.add_systems(Startup, init_assets).add_systems(Update, spawn_ball_on_click);
     }
 }
 
-struct Level {
-    color: Rgb,
-    radius: f32,
+struct Settings {
+    play_area_size: UVec2,
 }
 
-impl Level {
-    fn new(color: Color, radius: f32) -> Self {
-        Self { color, radius }
+#[derive(Component)]
+struct BallIndex(usize);
+
+impl std::ops::Deref for BallIndex {
+    type Target = usize;
+
+    fn deref(&self) -> &Self::Target {
+        self.0
     }
 }
 
 #[derive(Resource)]
-struct Stages(Vec<Vec<Level>>);
+struct BallLevels(Vec<(Color, f32)>);
 
-impl std::default::Default for Stages {
-    fn default() -> Self {
+impl std::ops::Deref for BallLevels {
+    type Target = Vec<(Color, f32)>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Resource)]
+struct BallMeshes(Vec<Handle<Mesh>>);
+
+impl BallMeshes {
+    // TODO: automatic updating of assets (based on lods)
+
+    fn init(mut commands: Commands, mut assets: ResMut<Assets<Mesh>>, mut levels: Res<BallLevels>) {
+        let radii = levels.iter().map(|(_, r)| r).collect::<HashSet>();
+        let handles = Vec::with_capacity(radii.len());
+
+        for radius in radii {
+            let mesh = shape::Circle {
+                radius,
+                vertices: 32 * (radius.powf(0.4) as usize),
+            };
+            handles.push(meshes.add(mesh));
+        }
+    }
+}
+
+#[derive(Resource)]
+struct Stages(Vec<(Handle<Mesh>, Handle<ColorMaterial>)>);
+
+impl std::ops::Index<BallIndex> for Stages {
+    type Output = Handle<Mesh>;
+
+    fn index(&self, index: BallIndex) -> Self::Output {
+        self.0[index].clone()
+    }
+}
+
+impl Stages {
+    fn init(mut commands: Commands, meshes: Res<BallMeshes>, mut materials: ResMut<Assets<ColorMaterial>>) {
         let mut stages = vec![vec![], vec![]];
-        let radius = |f: f32| (f / std::f32::consts::PI).sqrt();
+        let mut radii = [0.0; 10];
 
-        for f in (0..10).map(|i| i as f32) {
-            let level = Level::new(
-                Color::hsl(f * 36.0, 100.0, 50.0),
-                radius(f),
+        for i, r in radii.iter_mut().enumerate() {
+            *r = (i as f32 / std::f32::consts::PI).sqrt();
+        }
+        for i in 0..10 {
+            let ball = (
+                Color::hsl((i as f32) * 36.0, 100.0, 50.0),
+                radii[i],
             );
             stages[0].push(level);
         }
         for f in (0..10).map(|i| i as f32) {
-            let level = Level::new(
-                Color::hsl((f + 0.5) * 36.0, 100.0, 50.0),
-                radius(f),
+            let ball = (
+                Color::hsl(((i as f32) + 0.5) * 36.0, 100.0, 50.0),
+                radii[i],
             );
             stages[1].push(level);
         }
+        commands.insert_resource(Self(stages));
+    }
+}
+
+impl std::default::Default for Stages {
+    fn default() -> Self {
+        
         Self(stages)
     }
 }
@@ -92,18 +145,15 @@ fn spawn_ball_on_click(mut commands: Commands, mouse_input: Input<MouseButton>) 
 
 fn init_assets(
     mut assets: ResMut<Assets<Mesh>>,
-    mut handles: ResMut<BallMeshes>,
-    stages: Res<Stages>,
+    mut handles: ResMut<BallLods>,
 ) {
-    handles.balls = stages
-        .iter()
-        .map(|r| {
+    handles.balls = (0..20)
+        .map(|i| {
+            let vertices = 2 + (2.0).pow(i) as usize;
+
             assets
                 .add(
-                    shape::Circle {
-                        radius: 0.5,
-                        vertices: BALL_MIN_VERTICES * r.powf(0.4) as usize,
-                    }
+                    shape::Circle { radius: 1.0, vertices, }
                     .into(),
                 )
                 .into()
